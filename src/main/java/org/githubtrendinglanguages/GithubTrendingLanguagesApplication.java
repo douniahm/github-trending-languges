8f2c600d73d027696f03e3947b7503b0215ef8b3
douniahm <dounia.hm@hotmail.com>
2020-08-03 20:51:25 +0100

    first commit

---
+++ src/main/java/org/githubtrendinglanguages/GithubTrendingLanguagesApplication.java
@@ -0,0 +1,13 @@
+package org.githubtrendinglanguages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTrendingLanguagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTrendingLanguagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/githubtrendinglanguages/GithubTrendingLanguagesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.githubtrendinglanguages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTrendingLanguagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
