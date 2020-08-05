4822a1bcb9d4372c4063650baf0f29e86045a7c7
douniahm <dounia.hm@hotmail.com>
2020-08-05 04:34:50 +0100

    add language controller: - the controller receives HTTP GET request and then calls the service

---
+++ src/main/java/org/githubtrendinglanguages/language/LanguageController.java
@@ -0,0 +1,19 @@
+package org.githubtrendinglanguages.language;
+
+import java.util.HashMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class LanguageController {
+	@Autowired
+	LanguageService languageService;
+
+	@RequestMapping(value = "languages", method = RequestMethod.GET)
+	public HashMap<String, Language> getTopLanguages() {
+		return languageService.callGithubAPI();
+	}
+}
